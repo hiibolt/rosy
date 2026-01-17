ce5c22995754b6d2e2af43182fc23a9be578ba85
hiibolt <me@hiibolt.com>
2026-01-17 13:18:41 -0600

    refactor: Small changes to prettify `transpile.rs`

--- rosy/src/transpile.rs
+++ rosy/src/transpile.rs
@@ -2,21 +2,14 @@ use std::{any::Any, collections::{BTreeSet, HashMap}};
 use anyhow::{Result, Error};
 use crate::rosy_lib::RosyType;
 
-pub fn indent ( st: String ) -> String {
-    st.lines()
-        .map(|line| format!("\t{}", line))
-        .collect::<Vec<String>>()
-        .join("\n")
-}
-pub fn add_context_to_all ( arr: Vec<Error>, context: String ) -> Vec<Error> {
-    arr.into_iter()
-        .map(|err| err.context(context.clone()))
-        .collect()
-}
-
 pub trait TranspileWithType: Transpile + TypeOf + Send + Sync + std::fmt::Debug + Any + 'static {}
 pub trait TypeOf {
     fn type_of ( &self, context: &TranspilationInputContext ) -> Result<RosyType>;
+}
+pub trait Transpile: std::fmt::Debug {
+    fn transpile ( 
+        &self, context: &mut TranspilationInputContext
+    ) -> Result<TranspilationOutput, Vec<Error>>;
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -57,8 +50,18 @@ pub struct TranspilationOutput {
     pub serialization: String,
     pub requested_variables: BTreeSet<String>
 }
-pub trait Transpile: std::fmt::Debug {
-    fn transpile ( 
-        &self, context: &mut TranspilationInputContext
-    ) -> Result<TranspilationOutput, Vec<Error>>;
+
+
+// helper for indenting blocks
+pub fn indent ( st: String ) -> String {
+    st.lines()
+        .map(|line| format!("\t{}", line))
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+// helper for adding context to a vec of  errors
+pub fn add_context_to_all ( arr: Vec<Error>, context: String ) -> Vec<Error> {
+    arr.into_iter()
+        .map(|err| err.context(context.clone()))
+        .collect()
 }
