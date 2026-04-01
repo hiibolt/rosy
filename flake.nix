8bbb23de5a4bc954323ec0b23298c66d47711bbc
hiibolt <me@hiibolt.com>
2026-04-01 09:40:29 -0500

    chore: Updated build environment

--- flake.nix
+++ flake.nix
@@ -13,22 +13,18 @@
           overlays = [ rust-overlay.overlays.default ];
         };
         rustVersion = pkgs.rust-bin.nightly.latest.default;
-
         rustPlatform = pkgs.makeRustPlatform {
           cargo = rustVersion;
           rustc = rustVersion;
         };
-
         localRustBuild = rustPlatform.buildRustPackage rec {
           pname = "app";
           version = "0.0.1";
           src = ./.;
           cargoBuildFlags = "";
-
           cargoLock = {
             lockFile = ./Cargo.lock;
           };
-
           nativeBuildInputs = [ (rustVersion.override { extensions = ["rust-src"]; }) ] ++ (with pkgs; [
             pkg-config
             cargo
@@ -48,7 +44,6 @@
             libclang
             llvmPackages.libclang
           ];
-
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
@@ -57,9 +52,14 @@
       in
       {
         defaultPackage = localRustBuild;
-
         packages.cargo_setup = pkgs.mkShell {
           buildInputs = with pkgs; [ cargo rustc ];
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ localRustBuild ];
+          shellHook = ''
+            export PATH="$HOME/.bun/bin:$PATH"
+          '';
         };
     });
 }
